264aecd1ee4ac77a045dd0fa53820f3d66fdd8b0
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-21 15:38:54 +0530

    Create health-serve.component.ts

---
+++ src/app/health-serve/health-serve.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-serve',
+  imports: [],
+  templateUrl: './health-serve.component.html',
+  styleUrl: './health-serve.component.css'
+})
+export class HealthServeComponent {
+
+}
