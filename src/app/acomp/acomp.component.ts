9be49e857049857e70e5c50e31afb0403338bb92
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-21 14:44:48 +0530

    Create acomp.component.ts

---
+++ src/app/acomp/acomp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acomp',
+  imports: [],
+  templateUrl: './acomp.component.html',
+  styleUrl: './acomp.component.css'
+})
+export class AcompComponent {
+
+}
