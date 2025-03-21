d228e729fb46524b468cce6b964a654bc0d95be9
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-21 15:38:58 +0530

    Create TodoList.ts

---
+++ src/app/TodoList.ts
@@ -0,0 +1,13 @@
+export class TodoList{
+    id: number;
+    title: string;
+    description: string;
+    active: boolean;
+
+    constructor(id:number, title:string, description:string, active:boolean){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.active = active;
+    }
+}
