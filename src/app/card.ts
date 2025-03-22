111df29dd9ecdc5cdd66a85caa44d04dfa06bbc7
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-22 20:13:09 +0530

    Update card.ts

---
+++ src/app/card.ts
@@ -0,0 +1,16 @@
+export class card{
+    img: string;
+    title: string;
+    level : string;
+    intrest: string;
+    refund: string;
+    
+    constructor(img:string, title:string, level:string, intrest:string, refund:string){
+        this.img = img;
+        this.title = title;
+        this.level = level;
+        this.intrest = intrest;
+        this.refund = refund;
+    }
+ 
+}
