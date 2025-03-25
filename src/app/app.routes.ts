103eab456cff3a5e4b239cb0218721c91a25be0e
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-25 15:53:28 +0530

    all

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { ServicesComponent } from './navigation/services/services.component';
 import { ProductsComponent } from './navigation/products/products.component';
 import { AboutComponent } from './navigation/about/about.component';
 import { ContactComponent } from './navigation/contact/contact.component';
+import { HtmlComponent } from './strip/html/html.component';
+import { CssComponent } from './strip/css/css.component';
+import { JavascriptComponent } from './strip/javascript/javascript.component';
+import { SqlComponent } from './strip/sql/sql.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
@@ -11,5 +15,9 @@ export const routes: Routes = [
     {path:'services', component:ServicesComponent},
     {path:'products', component:ProductsComponent},
     {path:'about', component:AboutComponent},
-    {path:'contact', component: ContactComponent}
+    {path:'contact', component: ContactComponent},
+    {path:'html', component:HtmlComponent},
+    {path:'css', component:CssComponent},
+    {path:'js', component:JavascriptComponent},
+    {path:'sql', component:SqlComponent}
 ];
---
+++ src/app/strip/javascript/javascript.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-javascript',
+  imports: [],
+  templateUrl: './javascript.component.html',
+  styleUrl: './javascript.component.css'
+})
+export class JavascriptComponent {
+
+}
