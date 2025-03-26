6147ca6a562777cf82e6a98b60744ec59e30e386
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-26 15:01:18 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './navigation/home/home.component';
+import { ServicesComponent } from './navigation/services/services.component';
+import { ProductsComponent } from './navigation/products/products.component';
+import { AboutComponent } from './navigation/about/about.component';
+import { ContactComponent } from './navigation/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'home', component:HomeComponent},
+    {path:'services', component:ServicesComponent},
+    {path:'products', component:ProductsComponent},
+    {path:'about', component:AboutComponent},
+    {path:'contact', component: ContactComponent}
+
+];
